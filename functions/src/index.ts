5ed54b82b8bda6cf4f21a03e26684bd85e39bba9
Pedro Alvito <pedrotari7@gmail.com>
2021-05-10 09:34:28 +0200

    Working configuration of functions

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from "firebase-functions";
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  res.send(`
+      <!doctype html>
+      <head>
+        <title>Time</title>
+        <link rel="stylesheet" href="/style.css">
+        <script src="/script.js"></script>
+      </head>
+      <body>
+        <p>In London, the clock strikes:
+          <span id="bongs">${"BONG ".repeat(hours)}</span></p>
+        <button onClick="refresh(this)">Refresh</button>
+      </body>
+    </html>`);
+});
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  res.json({ bongs: "BONG ".repeat(hours) });
+});
+
+exports.app = functions.region("europe-west1").https.onRequest(app);
