d233ad320bbaf8b1e375b9fb43fd5cc0d31f81e4
Pedro Alvito <pedrotari7@gmail.com>
2021-05-26 11:03:17 +0200

    Tailwind purge config

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
+	purge: {
+		content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+
+		options: {
+			safelist: [],
+		},
+	},
+	darkMode: false,
 	theme: {
 		extend: {
 			colors: {
