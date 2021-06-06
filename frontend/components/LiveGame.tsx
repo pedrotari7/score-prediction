6e9ecc9165fb172a67ae670e7234158a0211dd6c
Pedro Alvito <pedrotari7@gmail.com>
2021-06-06 14:58:39 +0200

    Countdown for live game

--- frontend/components/LiveGame.tsx
+++ frontend/components/LiveGame.tsx
@@ -1,11 +1,14 @@
 import { DateTime } from 'luxon';
-import React, { ChangeEvent, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import Countdown, { zeroPad } from 'react-countdown';
 import FixturesContext from '../context/FixturesContext';
 import UserContext from '../context/UserContext';
 import { getCurrentDate } from '../lib/utils/reactHelper';
 import Flag from './Flag';
 
 const LiveGame = ({ gameID }: { gameID: number }) => {
+	const router = useRouter();
 	const data = useContext(FixturesContext);
 	const userInfo = useContext(UserContext);
 
@@ -19,28 +22,46 @@ const LiveGame = ({ gameID }: { gameID: number }) => {
 
 	const isInPast = getCurrentDate() < gameDate;
 
+	const timeDiff = gameDate.diff(getCurrentDate(), 'days').days;
+
+	const isCountdown = timeDiff <= 1;
+
 	return (
-		<div className="text-light flex flex-row items-center justify-evenly my-2 rounded p-2 bg-gark shadow-pop">
-			<span className="text-xs text-left w-2/12 flex ">
+		<div className="text-light flex flex-col sm:flex-row items-center sm:justify-evenly my-2 rounded p-2 bg-gark shadow-pop">
+			<span className="text-sm text-left sm:w-2/12 flex ">
 				<div className="flex items-center justify-center">
 					<span>{group}</span>
 				</div>
 			</span>
 
-			<div className="w-10/12 flex flex-row justify-center items-center">
+			<div className="w-10/12 flex flex-row justify-center items-center my-4">
 				<div className="flex flex-row items-center justify-end sm:w-4/12">
-					<span className="hidden sm:block mr-2">{game?.teams.home.name}</span>
+					<span className="text-xl hidden sm:block mr-2">{game?.teams.home.name}</span>
 					<Flag team={game?.teams.home} />
 				</div>
 
-				{isInPast && (
-					<span className="text-md sm:w-4/12 text-center mx-2">
+				{isInPast && isCountdown && (
+					<span className="text-3xl sm:w-4/12 text-center mx-2">
+						<Countdown
+							date={gameDate.toMillis()}
+							onComplete={() => router.push('/')}
+							renderer={({ hours, minutes, seconds }) => (
+								<span>
+									{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+								</span>
+							)}
+						/>
+					</span>
+				)}
+
+				{isInPast && !isCountdown && (
+					<span className="text-3xl sm:w-4/12 text-center mx-2">
 						{DateTime.fromISO(game?.fixture.date).toFormat('dd LLL HH:mm')}
 					</span>
 				)}
 
 				{!isInPast && (
-					<span className="text-md sm:w-4/12 text-center mx-2">
+					<span className="text-3xl sm:w-4/12 text-center mx-2">
 						<span>{game.goals.home}</span>
 						<span className="mx-2">-</span>
 						<span>{game.goals.away}</span>
@@ -49,11 +70,11 @@ const LiveGame = ({ gameID }: { gameID: number }) => {
 
 				<div className="flex flex-row items-center justify-start sm:w-4/12">
 					<Flag team={game?.teams.away} />
-					<span className="hidden sm:block ml-2">{game?.teams.away.name}</span>
+					<span className="text-xl hidden sm:block ml-2">{game?.teams.away.name}</span>
 				</div>
 			</div>
 
-			<span className="text-xs text-right w-0 md:w-2/12 invisible md:visible">
+			<span className="text-sm text-right sm:w-2/12">
 				{game?.fixture.venue.name}, {game?.fixture.venue.city}
 			</span>
 		</div>
