55ddb486e5fc9f3e0d75ae4a6bd17086cdfc15af
Pedro Alvito <pedrotari7@gmail.com>
2021-06-12 13:46:15 +0200

    Show more info component

--- frontend/components/LiveGame.tsx
+++ frontend/components/LiveGame.tsx
@@ -7,6 +7,7 @@ import UserContext from '../context/UserContext';
 import { getCurrentDate, isGameFinished } from '../lib/utils/reactHelper';
 import ClientOnly from './ClientOnly';
 import Flag from './Flag';
+import ShowMore from './ShowMore';
 
 const LiveGame = ({ gameID }: { gameID: number }) => {
 	const router = useRouter();
@@ -28,67 +29,69 @@ const LiveGame = ({ gameID }: { gameID: number }) => {
 	const isCountdown = timeDiff <= 1;
 
 	return (
-		<div className="text-light flex flex-col sm:flex-row items-center sm:justify-evenly my-2 rounded p-2 bg-gark shadow-pop">
-			<span className="text-sm text-left sm:w-2/12 flex ">
-				<div className="flex items-center justify-center">
-					<span>{group}</span>
-				</div>
-			</span>
+		<ShowMore className="text-light flex flex-col  my-2 rounded p-2 bg-gark shadow-pop">
+			<div className="flex flex-col sm:flex-row items-center sm:justify-evenly">
+				<span className="text-sm text-left sm:w-2/12 flex ">
+					<div className="flex items-center justify-center">
+						<span>{group}</span>
+					</div>
+				</span>
 
-			<div className="w-10/12 flex flex-row justify-evenly sm:justify-center items-center my-4">
-				<div className="flex flex-row items-center justify-end sm:w-4/12">
-					<span className="text-xl hidden sm:block mr-2">{game?.teams.home.name}</span>
-					<Flag team={game?.teams.home} />
-				</div>
+				<div className="w-10/12 flex flex-row justify-evenly sm:justify-center items-center my-4">
+					<div className="flex flex-row items-center justify-end sm:w-4/12">
+						<span className="text-xl hidden sm:block mr-2">{game?.teams.home.name}</span>
+						<Flag team={game?.teams.home} />
+					</div>
 
-				{isInPast && isCountdown && (
-					<span className="text-3xl sm:w-4/12 text-center mx-2">
-						<ClientOnly>
-							<Countdown
-								date={gameDate.toMillis()}
-								onComplete={() => router.push('/')}
-								renderer={({ hours, minutes, seconds }) => (
-									<span>
-										{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
-									</span>
+					{isInPast && isCountdown && (
+						<span className="text-3xl sm:w-4/12 text-center mx-2">
+							<ClientOnly>
+								<Countdown
+									date={gameDate.toMillis()}
+									onComplete={() => router.push('/')}
+									renderer={({ hours, minutes, seconds }) => (
+										<span>
+											{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+										</span>
+									)}
+								/>
+							</ClientOnly>
+						</span>
+					)}
+
+					{isInPast && !isCountdown && (
+						<span className="text-3xl sm:w-4/12 text-center mx-2">
+							{DateTime.fromISO(game?.fixture.date).toFormat('dd LLL HH:mm')}
+						</span>
+					)}
+
+					{!isInPast && (
+						<span className="text-3xl sm:w-4/12 text-center mx-2">
+							<div>
+								<span>{game.goals.home}</span>
+								<span className="mx-2">-</span>
+								<span>{game.goals.away}</span>
+							</div>
+							<div className="mt-2">
+								<span className="text-xs mm-1">{game.fixture.status.long}</span>
+								{!isGameFinished(game) && (
+									<span className="text-base mx-1">{game.fixture.status.elapsed}'</span>
 								)}
-							/>
-						</ClientOnly>
-					</span>
-				)}
+							</div>
+						</span>
+					)}
 
-				{isInPast && !isCountdown && (
-					<span className="text-3xl sm:w-4/12 text-center mx-2">
-						{DateTime.fromISO(game?.fixture.date).toFormat('dd LLL HH:mm')}
-					</span>
-				)}
-
-				{!isInPast && (
-					<span className="text-3xl sm:w-4/12 text-center mx-2">
-						<div>
-							<span>{game.goals.home}</span>
-							<span className="mx-2">-</span>
-							<span>{game.goals.away}</span>
-						</div>
-						<div className="mt-2">
-							<span className="text-xs mm-1">{game.fixture.status.long}</span>
-							{!isGameFinished(game) && (
-								<span className="text-base mx-1">{game.fixture.status.elapsed}'</span>
-							)}
-						</div>
-					</span>
-				)}
-
-				<div className="flex flex-row items-center justify-start sm:w-4/12">
-					<Flag team={game?.teams.away} />
-					<span className="text-xl hidden sm:block ml-2">{game?.teams.away.name}</span>
+					<div className="flex flex-row items-center justify-start sm:w-4/12">
+						<Flag team={game?.teams.away} />
+						<span className="text-xl hidden sm:block ml-2">{game?.teams.away.name}</span>
+					</div>
 				</div>
-			</div>
 
-			<span className="text-sm text-right sm:w-2/12">
-				{game?.fixture.venue.name}, {game?.fixture.venue.city}
-			</span>
-		</div>
+				<span className="text-sm text-right sm:w-2/12">
+					{game?.fixture.venue.name}, {game?.fixture.venue.city}
+				</span>
+			</div>
+		</ShowMore>
 	);
 };
 
---
+++ frontend/components/ShowMore.tsx
@@ -0,0 +1,32 @@
+import { Disclosure, Transition } from '@headlessui/react';
+import { ReactChild } from 'react';
+import { classNames } from '../lib/utils/reactHelper';
+import { DotsHorizontalIcon, ChevronUpIcon } from '@heroicons/react/outline';
+
+const ShowMore = ({ children, className }: { children: ReactChild; className: string }) => {
+	return (
+		<Disclosure as="div" className={classNames(className)}>
+			{({ open }) => (
+				<>
+					{children}
+
+					<Disclosure.Button className="focus:outline-none flex justify-center mt-4">
+						{!open && <DotsHorizontalIcon className="block h-6 w-6" aria-hidden="true" />}
+						{open && <ChevronUpIcon className="block h-6 w-6" aria-hidden="true" />}
+					</Disclosure.Button>
+					<Transition
+						enter="transition duration-100 ease-out"
+						enterFrom="transform scale-95 opacity-0"
+						enterTo="transform scale-100 opacity-100"
+						leave="transition duration-75 ease-out"
+						leaveFrom="transform scale-100 opacity-100"
+						leaveTo="transform scale-95 opacity-0">
+						{open && <div>More information</div>}
+					</Transition>
+				</>
+			)}
+		</Disclosure>
+	);
+};
+
+export default ShowMore;
