90b06319b46e066da1df7ac8b56879331cf712a1
Pedro Alvito <pedrotari7@gmail.com>
2021-06-13 14:29:56 +0200

    Halftime result in gamefacts

--- frontend/components/GameFacts.tsx
+++ frontend/components/GameFacts.tsx
@@ -13,6 +13,8 @@ const GameFacts = ({ game }: { game: Fixture }) => {
 	const Event = ({ event }: { event: Event }) => {
 		const isAwayTeam = event.team.id === game.teams.away.id;
 
+		const TimeElapsed = () => <span className="mx-2 font-bold w-5 text-center">{event.time.elapsed}'</span>;
+
 		const EventContainer = ({ children }: { children: ReactNode }) => {
 			return (
 				<div
@@ -33,7 +35,7 @@ const GameFacts = ({ game }: { game: Fixture }) => {
 
 			return (
 				<EventContainer>
-					<span className="mx-2 font-bold">{event.time.elapsed}'</span>
+					<TimeElapsed />
 					{isNormalGoal && <img className="mx-2 h-5 w-5" src="/events/goal.svg" />}
 					{isOwnGoal && <img className="mx-2 h-5 w-5" src="/events/own_goal.svg" />}
 					{isMissedPenalty && <img className="mx-2 h-5 w-5" src="/events/missed_penalty.svg" />}
@@ -48,7 +50,7 @@ const GameFacts = ({ game }: { game: Fixture }) => {
 		} else if (event.type === EventType.Subst) {
 			return (
 				<EventContainer>
-					<span className="mx-2 font-bold">{event.time.elapsed}'</span>
+					<TimeElapsed />
 					<img
 						className="mx-2 h-5 w-5"
 						src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAATlBMVEVHcEzkXVoAl17kXVkAl1/kXVoAl1zkWlrkXlrlXVkAl18An18AmV8Al17kXFrlXVvkXFoAl14Al14Al17kXVoAmF3kXVoAmF7lXlsAmF/fU5yDAAAAGHRSTlMA76DgQPBgMH+AIBBQwGBwwO/wr7+Q37C91jpQAAAAk0lEQVQ4y+XU2Q4CIQyF4cKAuI+OW/v+L+qF0YSlyX9r5lx/obQ0iPx/0pG5g9mGHWhQJoMyGZQfOJD729VG6eTJnLTS3NATH+0dX8vQRdh1hOOJcI4RvkzlyvPubU/tgirZxxLUg9I5pU6p022d4rku5wZOjtNAYW5LZ1j6J3dV5jJoO7PxfKVQKVSyz6hMF1lR3lY7HudLRmFoAAAAAElFTkSuQmCC"
@@ -66,7 +68,7 @@ const GameFacts = ({ game }: { game: Fixture }) => {
 
 			return (
 				<EventContainer>
-					<span className="mx-2 font-bold">{event.time.elapsed}'</span>
+					<TimeElapsed />
 					{isYellowCard && <img className="mx-3 h-5 w-3" src="/events/yellow_card.svg" />}
 					{isSecondYellowCard && <img className="mx-3 h-5 w-3" src="/events/yellow_card.svg" />}
 					{isRedCard && <img className="mx-3 h-5 w-3" src="/events/red_card.svg" />}
@@ -78,13 +80,35 @@ const GameFacts = ({ game }: { game: Fixture }) => {
 		return <></>;
 	};
 
-	const events = isGameFinished(game) ? game?.events?.reverse() : game?.events;
+	const HalfTimeScore = () => {
+		if (game.score.halftime.home === null || game.score.halftime.away) return <></>;
+		return (
+			<div className="text-sm flex flex-row items-center">
+				<div className="flex flex-grow bg-gray-500 h-0.5 opacity-80 "></div>
+				<div className="w-max mx-8">
+					HT {game.score.halftime.home} - {game.score.halftime.away}
+				</div>
+				<div className="flex flex-grow bg-gray-500 h-0.5 opacity-80 "></div>
+			</div>
+		);
+	};
+
+	const events = game?.events?.map((event, idx) => {
+		const shouldAddHTScore = !addedHTScore && event.time.elapsed >= 45;
+		if (shouldAddHTScore) addedHTScore = shouldAddHTScore;
+		return (
+			<>
+				{shouldAddHTScore && <HalfTimeScore />}
+				<Event key={idx} event={event} />
+			</>
+		);
+	});
+
+	let addedHTScore = false;
 
 	return (
 		<div className="bg-gray-700 rounded-md p-2 flex flex-col xl:items-center justify-center">
-			{events?.map((event, idx) => (
-				<Event key={idx} event={event} />
-			))}
+			{isGameFinished(game) ? events : events.reverse()}
 		</div>
 	);
 };
