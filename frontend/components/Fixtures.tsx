78109e26153854e1c0fa979614fb11c73d812ea8
Pedro Alvito <pedrotari7@gmail.com>
2021-06-22 14:15:15 +0200

    Different rounds on the fixtures page

--- frontend/components/Fixtures.tsx
+++ frontend/components/Fixtures.tsx
@@ -1,6 +1,6 @@
-import { Fixtures, Prediction, Predictions, Standings, User } from '../../interfaces/main';
+import { Fixture, Fixtures, Prediction, Predictions, Standings, User } from '../../interfaces/main';
 import { useAuth } from '../lib/auth';
-import { classNames } from '../lib/utils/reactHelper';
+import { classNames, isGameFinished } from '../lib/utils/reactHelper';
 import Game from './Game';
 import PredictedGroups from './PredictedGroups';
 
@@ -21,10 +21,43 @@ const FixturesPage = ({
 
 	const uid = currentUser?.uid;
 
-	const sortedFixtures = Object.values(fixtures).sort((a, b) => a.fixture.timestamp - b.fixture.timestamp);
+	const isGroupStage = (f: Fixture) => f.league.round.includes('Group');
+
+	const groupStageFixtures = Object.values(fixtures).filter((f: Fixture) => isGroupStage(f));
+
+	const otherStageFixtures = Object.values(fixtures)
+		.filter((f: Fixture) => !isGroupStage(f))
+		.reduce<Record<string, Fixture[]>>((rounds, f: Fixture) => {
+			const round = f.league.round;
+
+			if (!(round in rounds)) rounds[round] = [];
+
+			rounds[round].push(f);
+
+			return rounds;
+		}, {});
+
+	const sortWithFinishedLast = (a: Fixture, b: Fixture) => {
+		if (isGameFinished(a) && isGameFinished(b)) return b.fixture.timestamp - a.fixture.timestamp;
+		if (isGameFinished(a) && !isGameFinished(b)) return 1;
+		if (!isGameFinished(a) && isGameFinished(b)) return -1;
+		return a.fixture.timestamp - a.fixture.timestamp;
+	};
+
+	const sortedGroupStageFixtures = groupStageFixtures.sort(sortWithFinishedLast);
+
+	const GameFilled = (game: Fixture, index: number) => (
+		<Game
+			predictions={predictions}
+			gameID={game.fixture?.id}
+			updatePrediction={async (update: Prediction) => await updatePrediction(update, game.fixture?.id)}
+			userID={user.uid}
+			key={index}
+		/>
+	);
 
 	return (
-		<main
+		<div
 			className={classNames(
 				'flex flex-col justify-center select-none text-light m-8 p-8 shadow-pop rounded-md bg-dark',
 				'mx-2 md:mx-24 lg:mx-48'
@@ -35,29 +68,31 @@ const FixturesPage = ({
 				{uid === user.uid && <p>My Predictions</p>}
 			</div>
 
-			<div className="flex flex-col-reverse md:flex-col">
-				<PredictedGroups
-					predictions={predictions}
-					fixtures={fixtures}
-					standings={standings}
-					userID={user.uid}
-				/>
+			{Object.entries(otherStageFixtures).map(([round, games]) => {
+				games.sort(sortWithFinishedLast);
+				return (
+					<div key={round} className="mb-6">
+						<div className="text-3xl mb-6">{round}</div>
+						{games.map(GameFilled)}
+					</div>
+				);
+			})}
 
-				<div className="flex flex-col">
-					{sortedFixtures.map((game, index) => (
-						<Game
-							predictions={predictions}
-							gameID={game.fixture?.id}
-							updatePrediction={async (update: Prediction) =>
-								await updatePrediction(update, game.fixture?.id)
-							}
-							key={index}
-							userID={user.uid}
-						/>
-					))}
+			<div className="flex flex-col">
+				<div className="text-3xl mb-6">Group Stage</div>
+
+				<div className="flex flex-col-reverse md:flex-col">
+					<PredictedGroups
+						predictions={predictions}
+						fixtures={fixtures}
+						standings={standings}
+						userID={user.uid}
+					/>
+
+					<div className="flex flex-col">{sortedGroupStageFixtures.map(GameFilled)}</div>
 				</div>
 			</div>
-		</main>
+		</div>
 	);
 };
 
