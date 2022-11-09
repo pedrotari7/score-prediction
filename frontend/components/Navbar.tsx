4f9caba3a4921d76cfb11f7762207fb126f184f7
Pedro Alvito <pedrotari7@gmail.com>
2022-11-09 19:12:41 +0100

    remove lingering login redirect

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -166,7 +166,6 @@ export default function Navbar({ loading }: { loading: boolean }) {
 																href=""
 																onClick={async () => {
 																	await getAuth(app).signOut();
-																	router.replace('/login');
 																}}
 																className={classNames(
 																	active ? 'bg-gray-100' : '',
