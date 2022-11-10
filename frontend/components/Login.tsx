3ae9768ef53b3b5779ed8e6bbfc1016b4239f22c
Pedro Alvito <pedrotari7@gmail.com>
2022-11-10 22:08:44 +0100

    login intro video

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -5,7 +5,7 @@ import { app } from '../lib/firebaseClient';
 import { Dispatch, SetStateAction } from 'react';
 
 const Login = ({ setSuccessfulLogin }: { setSuccessfulLogin: Dispatch<SetStateAction<boolean>> }) => {
-	const uiConfig = {
+	const uiConfig: firebaseui.auth.Config = {
 		signInOptions: [GoogleAuthProvider.PROVIDER_ID],
 		callbacks: {
 			// Avoid redirects after sign-in.
@@ -23,7 +23,7 @@ const Login = ({ setSuccessfulLogin }: { setSuccessfulLogin: Dispatch<SetStateAc
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
-			{/* <video className="absolute object-cover h-full" src="/sample.m4v" muted autoPlay></video> */}
+			<video className="absolute object-cover h-full" src="/sample.m4v" muted autoPlay></video>
 			<div className="flex flex-col items-center justify-evenly absolute w-screen h-full bg-[#242424] bg-opacity-90">
 				<div className="text-light text-4xl md:text-8xl font-bold text-center">SCORE PREDICTION</div>
 				<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={getAuth(app)} />
