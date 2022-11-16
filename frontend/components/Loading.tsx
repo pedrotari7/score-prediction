adf80844ed6fc34c3d7334cf8f3f92e44cc91776
Pedro Alvito <pedrotari7@gmail.com>
2022-11-16 11:52:16 +0100

    decrease size of the loading message

--- frontend/components/Loading.tsx
+++ frontend/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = ({ message }: { message?: string }) => (
 	<div className="w-full h-full flex flex-col items-center justify-center text-white">
-		<div className="text-4xl">{message}</div>
+		<div className="text-3xl">{message}</div>
 		<img className="" src="loader.svg" />
 	</div>
 );
