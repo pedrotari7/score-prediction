d3fcff443dc8552bf05e9604894d667d9662cdac
Pedro Alvito <pedrotari7@gmail.com>
2022-11-27 16:17:12 +0100

    change color and padding of noSpoilersToggle

--- frontend/components/NoSpoilersToggle.tsx
+++ frontend/components/NoSpoilersToggle.tsx
@@ -11,12 +11,13 @@ const NoSpoilersToggle = () => {
 	return (
 		<label
 			className={classNames(
-				'flex flex-col items-center justify-between gap-2 rounded-md p-2 text-xs font-bold sm:p-4',
+				'flex flex-col items-center justify-between gap-2 rounded-md py-2 text-xs font-bold sm:py-4',
 				'cursor-pointer'
 			)}
 		>
 			<Switch
 				onChange={setNoSpoilers}
+				onColor='#181a1b'
 				uncheckedIcon={
 					<div className='flex h-full w-full items-center justify-center'>
 						<EyeIcon strokeWidth={2} className='h-4 w-4 text-white' />
