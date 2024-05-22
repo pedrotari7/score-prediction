1615d8ab82c6bfad006d9adc4e0036befb94c1d5
Pedro Alvito <pedrotari7@gmail.com>
2024-05-22 10:39:06 +0200

    only frontend changes

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -26,7 +26,7 @@ const Login = () => {
 			<video className='absolute h-full w-full object-cover' src='/sample.mp4' muted autoPlay loop></video>
 			<div className='absolute flex h-full w-screen flex-col items-center justify-evenly bg-[#181a1b] bg-opacity-90'>
 				<div className='text-center text-4xl font-bold text-light md:text-8xl'>SCORE PREDICTION</div>
-				{isAllowedBrowser && (
+				{isAllowedBrowser ? (
 					<StyledFirebaseAuth
 						uiConfig={{
 							signInOptions: [GoogleAuthProvider.PROVIDER_ID],
@@ -38,8 +38,7 @@ const Login = () => {
 						}}
 						firebaseAuth={getAuth(app)}
 					/>
-				)}
-				{!isAllowedBrowser && (
+				) : (
 					<div className='flex flex-row items-center gap-2 rounded-md bg-red-200 p-4 text-2xl'>
 						<ExclamationTriangleIcon className='h-10 w-10 font-bold text-red-800' />
 						<span className='text-red-800'> Browser not supported </span>
