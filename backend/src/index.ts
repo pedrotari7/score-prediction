4116a6148e6733d381d943dc56b9d9438f03dc82
Pedro Alvito <pedrotari7@gmail.com>
2024-06-12 18:49:45 +0200

    fix: remove header validation of rate limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,6 +50,7 @@ import { rateLimit } from 'express-rate-limit';
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // max 100 requests per windowMs
+  validate: { xForwardedForHeader: false },
 });
 
 const app = express();
