345549a622f50a3c57bdcdd892057939b5bffff2
Pedro Alvito <pedrotari7@gmail.com>
2025-01-19 21:30:01 +0100

    fix: revert express types to latest express version

--- frontend/hooks/useNoSpoilers.tsx
+++ frontend/hooks/useNoSpoilers.tsx
@@ -1,4 +1,5 @@
 import { EyeSlashIcon } from '@heroicons/react/24/outline';
+import type { JSX } from 'react';
 import { useContext } from 'react';
 import NoSpoilersContext from '../context/NoSpoilersContext';
 import { classNames } from '../lib/utils/reactHelper';
@@ -21,7 +22,7 @@ const useNoSpoilers = () => {
 	}) => {
 		return noSpoilers ? (
 			<div className={classNames('flex flex-row items-center justify-center gap-2', className)}>
-				{withIcon && <EyeSlashIcon className={classNames('h-8 w-8', iconStyle)} />}
+				{withIcon && <EyeSlashIcon className={classNames('size-8', iconStyle)} />}
 				{message && <span>{message}</span>}
 			</div>
 		) : (
